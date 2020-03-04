da45741ba2a58dfb7dded3e7a7bb654b3f78c02f
Student <student@richmw34579bel.rsd38.ca>
2020-03-03 20:13:09 -0800

    pie chart initial

--- app/src/main/java/com/example/app/MainActivity.java
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -6,6 +6,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -30,4 +33,46 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, CameraActivity.class);
         startActivity(intent);
     }
+
+    int calsBurned = 0;
+    int calsConsumed = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    public void addBurned(View v) {
+        // Get the new value from a user input:
+        EditText burnedEditText = findViewById(R.id.burned);
+
+        // Update the old value:
+        calsBurned = Integer.parseInt(burnedEditText.getText().toString());
+        updateChart();
+    }
+
+    public void addConsumed(View v) {
+        // Get the new value from a user input:
+        EditText consumedEditText = findViewById(R.id.consumed);
+
+        // Update the old value:
+        calsConsumed = Integer.parseInt(consumedEditText.getText().toString());
+        updateChart();
+    }
+
+    private void updateChart(){
+        // Update the text in a center of the chart:
+        TextView numberOfCals = findViewById(R.id.number_of_calories);
+        numberOfCals.setText(String.valueOf(calsBurned) + " / " + calsConsumed);
+
+        // Calculate the slice size and update the pie chart:
+        ProgressBar pieChart = findViewById(R.id.stats_progressbar);
+        double d = (double) calsBurned / (double) calsConsumed;
+        int progress = (int) (d * 100);
+        pieChart.setProgress(progress);
+    }
+}
+
 }
