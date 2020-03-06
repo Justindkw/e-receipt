28df40801d69ac08f0d0e320d528948557ce92ca
advantus <delsinpearce123@gmail.com>
2020-03-06 12:09:43 -0800

    home screen buttons work

--- app/src/main/java/com/example/app/homeScreen.java
+++ app/src/main/java/com/example/app/homeScreen.java
@@ -2,7 +2,10 @@ package com.example.app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class homeScreen extends AppCompatActivity {
 
@@ -10,5 +13,38 @@ public class homeScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
+
+        statisticsButton = findViewById(R.id.statisticsButton);
+        statisticsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //User taps on statisticsButton
+                toStatsScreen();
+            }
+        });
+
+        budgetButton = findViewById(R.id.budgetingButton);
+        budgetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toBudgetScreen();
+            }
+        });
+    }
+
+    //Buttons
+
+    Button statisticsButton;
+    Button budgetButton;
+
+    //Private button voids
+    private void toStatsScreen() {
+        Intent toStatsScreen = new Intent(this, statistics.class);
+        startActivity(toStatsScreen);
+    }
+
+    private void toBudgetScreen() {
+        Intent toBudgetScreen = new Intent(this, budgeting.class);
+        startActivity(toBudgetScreen);
     }
 }
