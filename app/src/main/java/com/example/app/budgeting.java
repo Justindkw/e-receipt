0db88c1d0d0f4ef02e0384fcee399487ad58df3c
advantus <delsinpearce123@gmail.com>
2020-03-06 12:25:46 -0800

    added buttons to stats and budgeting screen

--- app/src/main/java/com/example/app/budgeting.java
+++ app/src/main/java/com/example/app/budgeting.java
@@ -2,7 +2,10 @@ package com.example.app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class budgeting extends AppCompatActivity {
 
@@ -10,5 +13,36 @@ public class budgeting extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_budgeting);
+
+        homeButton = findViewById(R.id.homeButton);
+        homeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toHomeScreen();
+            }
+        });
+
+        statisticsButton = findViewById(R.id.statisticsButton);
+        statisticsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toStatsScreen();
+            }
+        });
+    }
+
+    //Buttons
+    Button homeButton;
+    Button statisticsButton;
+
+    //Private button voids
+    private void toHomeScreen() {
+        Intent toHomeScreen = new Intent(this, homeScreen.class);
+        startActivity(toHomeScreen);
+    }
+
+    private void toStatsScreen() {
+        Intent toStatsScreen = new Intent(this, homeScreen.class);
+        startActivity(toStatsScreen);
     }
 }
--- app/src/main/java/com/example/app/statistics.java
+++ app/src/main/java/com/example/app/statistics.java
@@ -2,7 +2,10 @@ package com.example.app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class statistics extends AppCompatActivity {
 
@@ -10,5 +13,36 @@ public class statistics extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_statistics);
+
+        homeButton = findViewById(R.id.homeButton);
+        homeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toHomeScreen();
+            }
+        });
+
+        budgetButton = findViewById(R.id.budgetingButton);
+        budgetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toBudgetScreen();
+            }
+        });
+    }
+
+    //Buttons
+    Button homeButton;
+    Button budgetButton;
+
+    //Private button voids
+    private void toHomeScreen() {
+        Intent toHomeScreen = new Intent(this, homeScreen.class);
+        startActivity(toHomeScreen);
+    }
+
+    private void toBudgetScreen() {
+        Intent toBudgetScreen = new Intent(this, budgeting.class);
+        startActivity(toBudgetScreen);
     }
 }
