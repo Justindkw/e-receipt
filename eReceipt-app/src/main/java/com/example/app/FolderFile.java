0ee59dbf5d4ef31403e1202b98a3b7f2752b3dd4
advantus <delsinpearce123@gmail.com>
2020-04-19 12:16:12 -0700

    just moved some stuff

--- eReceipt-app/src/main/java/com/example/app/FolderFile.java
+++ eReceipt-app/src/main/java/com/example/app/FolderFile.java
@@ -9,34 +9,12 @@ import android.widget.TextView;
 
 public class FolderFile extends AppCompatActivity {
 
-    private static  final String TAG = "folderFile";
-
     private Folder folder; //not Lucas' stuff
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_folder_file);
-        Log.d(TAG, "onCreate: started.");
         folder = getIntent().getParcelableExtra("folder"); //not Lucas' stuff
     }
 
-
-    private void getIncomingIntent() {
-        Log.d(TAG, "getIncomingIntent: checking for incoming intents"); //Log.d is just to check if the method is working
-
-        if(getIntent().hasExtra("name")) {
-            Log.d(TAG, "getIncomingIntent: found intent extras");
-
-            String folderName = getIntent().getStringExtra("name");
-
-
-        }
-    }
-
-    private void setName(String folderName) {
-        Log.d(TAG, "setName: setting the name of the folder to widgets");
-
-        TextView name = findViewById(R.id.info_text); //change info_text to something in folder_file.xml MUST DO
-        name.setText(folderName);
-    }
 }
--- eReceipt-app/src/main/java/com/example/app/receiptFolder.java
+++ eReceipt-app/src/main/java/com/example/app/receiptFolder.java
@@ -4,17 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 
 public class receiptFolder extends AppCompatActivity {
     static final int REQUEST_RECIEPT = 1;
     //GlobalFolderList folderList = (GlobalFolderList)getApplication();
+
+    private static  final String TAG = "folderFile"; //Lucas' stuff
+
     private String name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_receipt_folder);
+        Log.d(TAG, "onCreate: started.");
         name = getIntent().getParcelableExtra("folderName");
     }
     private void toAddFolder(){
@@ -30,4 +36,24 @@ public class receiptFolder extends AppCompatActivity {
             }
         }
     }
+    //Lucas' stuff starts here
+    //This sets the folder's name depending on what folder is clicked in the RecyclerView
+    private void getIncomingIntent() {
+        Log.d(TAG, "getIncomingIntent: checking for incoming intents"); //Log.d is just to check if the method is working
+
+        if(getIntent().hasExtra("name")) {
+            Log.d(TAG, "getIncomingIntent: found intent extras");
+
+            String folderName = getIntent().getStringExtra("name");
+
+
+        }
+    }
+
+    private void setName(String folderName) {
+        Log.d(TAG, "setName: setting the name of the folder to widgets");
+
+        TextView name = findViewById(R.id.info_text); //change info_text to something in folder_file.xml MUST DO
+        name.setText(folderName);
+    }//Lucas' stuff ends here
 }
